21cf1b52a4c5696d677ca6ccfb89ffb1a1953df9
Blagovest Tsolov <bobytsolov@gmail.com>
2024-03-28 16:45:25 +0200

    controllers constructors added

--- QuizGame/Controllers/AuthorController.cs
+++ QuizGame/Controllers/AuthorController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuizGame.Core.Contracts;
 
 namespace QuizGame.Controllers
 {
     public class AuthorController : BaseController
     {
+        private readonly IAuthorService authorService;
+
+        public AuthorController(IAuthorService _authorService)
+        {
+            authorService = _authorService;
+        }
+
         public async Task<IActionResult> Become()
         {
             return View();
--- QuizGame/Controllers/QuizController.cs
+++ QuizGame/Controllers/QuizController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuizGame.Core.Contracts;
 
 namespace QuizGame.Controllers
 {
     public class QuizController : BaseController
     {
+        private readonly IQuizService quizService;
+
+        public QuizController(IQuizService _quizService)
+        {
+            quizService = _quizService;
+        }
+
         public IActionResult Index()
         {
             return View();
