00244ee03f81bd8e494be8d94deccb6e0e2b3f21
Blagovest Tsolov <bobytsolov@gmail.com>
2024-04-17 16:59:55 +0300

    UserService tests added

---
+++ QuizGame.UnitTests/UnitTests/UnitTestsBase.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Identity;
+using QuizGame.Data;
+using QuizGame.Infrastructure.Data.Models;
+using QuizGame.Infrastructure.Repository;
+using QuizGame.UnitTests.Mocks;
+
+namespace QuizGame.UnitTests.UnitTests
+{
+    public class UnitTestsBase
+    {
+        protected IRepository context;
+
+        public Author Author { get; set; }
+
+        public Quiz Quiz { get; set; }
+
+        public Trivia Trivia { get; set; }
+
+        [OneTimeSetUp]
+        public void Seed()
+        {
+            context = DatabaseMock.Instance;
+            SeedDatabase();
+        }
+
+        private async Task SeedDatabase()
+        {
+            Author = new Author()
+            {
+                Id = 1,
+                UserId = "AuthorId"
+            };
+
+            await context.AddAsync(Author);
+
+            Quiz = new Quiz()
+            {
+                Id = 1,
+                Author = Author,
+                QuestionType = new QuestionType { Name = "History" },
+                Question = "Question",
+                Answer = "Answer"
+            };
+
+            await context.AddAsync(Quiz);
+            
+            Trivia = new Trivia()
+            {
+                Id = 1,
+                Author = Author,
+                Category = new Category { Name = "Tech" },
+                Comment = "Comment"
+            };
+
+            await context.AddAsync(Trivia);
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ QuizGame.UnitTests/UnitTests/UserServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using QuizGame.Core.Contracts;
+using QuizGame.Core.Services;
+
+namespace QuizGame.UnitTests.UnitTests
+{
+    [TestFixture]
+    public class UserServiceTests : UnitTestsBase
+    {
+        private IUserService userService;
+
+        [OneTimeSetUp]
+        public void SetUp()
+            => userService = new UserService(context);
+
+        [Test]
+        public async Task GetAllUsersAsync_ShouldReturnCorrectData()
+        {
+            var result = (await userService.GetAllUsersAsync()).Count();
+            var usersCount = (await context.AllReadOnlyAsync<IdentityUser>()).Count();
+
+            Assert.That(result, Is.EqualTo(usersCount));
+        }
+    }
+}
