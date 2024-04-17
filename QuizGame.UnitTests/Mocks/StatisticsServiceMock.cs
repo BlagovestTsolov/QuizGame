1d09012b1d6c5fa0a7fc83ed278d428fedb05253
Blagovest Tsolov <bobytsolov@gmail.com>
2024-04-17 21:40:19 +0300

    StatisticsServiceMock added

---
+++ QuizGame.UnitTests/Mocks/StatisticsServiceMock.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using QuizGame.Core.Contracts;
+using QuizGame.Core.Models.Statistic;
+
+namespace QuizGame.UnitTests.Mocks
+{
+    public class StatisticsServiceMock
+    {
+        public static IStatisticService Instance
+        {
+            get
+            {
+                var statisticsServiceMock = new Mock<IStatisticService>();
+
+                statisticsServiceMock
+                   .Setup(s => s.TotalAsync())
+                   .Returns(async () => new StatisticModel()
+                   {
+                       TotalAuthors = 1,
+                       TotalQuizzes = 3,
+                       TotalTrivias = 3
+                   });
+
+                return statisticsServiceMock.Object;
+            }
+        }
+    }
+}
