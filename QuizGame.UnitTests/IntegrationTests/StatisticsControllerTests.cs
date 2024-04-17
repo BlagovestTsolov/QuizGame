734e877eaa74db05b14d0a557aaf6f2d03b6e4a6
Blagovest Tsolov <bobytsolov@gmail.com>
2024-04-17 22:05:09 +0300

    StatisticsControllerTests added

---
+++ QuizGame.UnitTests/IntegrationTests/StatisticsControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizGame.Controllers;
+using QuizGame.UnitTests.Mocks;
+
+namespace QuizGame.UnitTests.IntegrationTests
+{
+    [TestFixture]
+    public class StatisticsControllerTests
+    {
+        private StatisticController statisticController;
+
+        [OneTimeSetUp]
+        public void SetUp()
+            => statisticController = new StatisticController(StatisticsServiceMock.Instance);
+
+        [Test]
+        public async Task GetStatistics_ShouldReturnCorrectCounts()
+        {
+            IActionResult result = await statisticController.GetStatistics();
+
+            Assert.NotNull(result);
+        }
+    }
+}
